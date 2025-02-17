d1812329f45121db9159d9cc31ebbc60c95bf592
roisin-m <s00223865@atu.ie>
2025-02-17 15:07:43 +0000

    unit tests for compute.ts

---
+++ src/app/bascomputics/compute.spec.ts
@@ -0,0 +1,15 @@
+import { compute } from "./compute";
+
+//create the spec - describes the suite of tests
+describe('Compute', ()=> {
+	//name of the spec
+    it('should return 0 if the input is negative',() =>{ 
+        //if parameter is 2 then test will fail as it doesn't equal 0
+        const result = compute(-1);
+        expect (result).toBe(0);
+    });
+    it('should return input+1 if the input is positive',() =>{ 
+        const result = compute(1);
+        expect (result).toBe(2);
+    });
+})
